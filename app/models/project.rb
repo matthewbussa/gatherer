60aad2c83878016cd581608e74b52b9ea1b1c2d6
Matthew Bussa <matthewbussa@gmail.com>
2016-03-04 22:49:53 -0500

    distinguising a completed task

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,24 @@
 class Project
-  def done?
-    true
+  attr_accessor :tasks
+
+  def initialize()
+    @tasks = []
   end
+
+  def incomplete_tasks
+    tasks.reject(&:complete?)
+  end
+
+  def done?
+    incomplete_tasks.empty?
+  end
+
+  def total_size
+    tasks.sum(&:size)
+  end
+
+  def remaining_size
+    incomplete_tasks.sum(&:size)
+  end
+
 end
