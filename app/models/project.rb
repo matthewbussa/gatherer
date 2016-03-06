80be1a1bc1b96558f8efd48aa758b8ee27d89e70
Matthew Bussa <matthewbussa@gmail.com>
2016-03-05 21:18:42 -0500

    finished up project and task model specs

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project
-  attr_accessor :tasks
+  attr_accessor :tasks, :due_date
 
   def initialize()
     @tasks = []
@@ -10,7 +10,7 @@ class Project
   end
 
   def done?
-    incomplete_tasks.empty?
+    tasks.reject(&:complete?).empty?
   end
 
   def total_size
@@ -19,6 +19,27 @@ class Project
 
   def remaining_size
     incomplete_tasks.sum(&:size)
+  end
+
+  def completed_velocity
+    tasks.sum(&:points_toward_velocity)
+  end
+
+  def current_rate
+    completed_velocity * 1.0 / Project.velocity_length_in_days
+  end
+
+  def projected_days_remaining
+    remaining_size / current_rate
+  end
+
+  def on_schedule?
+    return false if projected_days_remaining.nan?
+    (Date.today + projected_days_remaining) <= due_date
+  end
+
+  def self.velocity_length_in_days
+    21
   end
 
 end
