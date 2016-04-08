8ee6e44d8e5c89353f32509033b461f944bb7a4a
Matthew Bussa <matthewbussa@gmail.com>
2016-04-07 22:11:03 -0400

    added update method

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -15,5 +15,20 @@ RSpec.describe ProjectsController, type: :controller do
       expect(assigns(:project)).to be_present
     end
 
+    it "fails create gracefully" do
+      action_stub = double(create: false, project: Project.new)
+      expect(CreatesProject).to receive(:new).and_return(action_stub)
+
+      post :create, :project => {name: 'Project Runway'}
+      expect(response).to render_template(:new)
+    end
+
+    it "fails update gracefully" do
+      sample = Project.create!(name: "Test Project")
+      expect(sample).to receive(:update_attributes).and_return(false)
+      allow(Project).to receive(:find).and_return(sample)
+      patch :update, id: sample.id, project: {name: "Fred" }
+      expect(response).to render_template(:edit)
+    end
   end
 end
